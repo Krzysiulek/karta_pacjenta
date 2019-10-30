a0351467026f2cf4d959e22497ea50d6d09b11ab
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-10-30 12:42:26 +0100

    init commit

---
+++ src/test/java/karta_pacjanta/karta/KartaApplicationTests.java
@@ -0,0 +1,13 @@
+package karta_pacjanta.karta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
