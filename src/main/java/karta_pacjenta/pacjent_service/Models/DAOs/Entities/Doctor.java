39b96c1c657f6803ad3807c70f7e528f5b06ce28
Błażej Czekała <blazej.czekala@gmail.com>
2019-11-19 22:30:52 +0100

    todo's added

--- src/main/java/karta_pacjenta/pacjent_service/Models/DAOs/Entities/Doctor.java
+++ src/main/java/karta_pacjenta/pacjent_service/Models/DAOs/Entities/Doctor.java
@@ -1,8 +1,22 @@
 package karta_pacjenta.pacjent_service.Models.DAOs.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.util.List;
 
+/**
+ * TODO
+ */
+
+@Entity
+@Data
+@Table(name = "doctors")
+@AllArgsConstructor
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long doctorId;
     private long userId;
 
--- src/main/java/karta_pacjenta/pacjent_service/Models/DAOs/Entities/MedicalFacility.java
+++ src/main/java/karta_pacjenta/pacjent_service/Models/DAOs/Entities/MedicalFacility.java
@@ -1,5 +1,17 @@
 package karta_pacjenta.pacjent_service.Models.DAOs.Entities;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * TODO
+ */
+@Entity
+@Data
+@Table(name = "MedicalFacilities")
 public class MedicalFacility {
     private long id;
     private String name;
