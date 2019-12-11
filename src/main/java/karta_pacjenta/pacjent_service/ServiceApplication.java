fdb0b14c6e1a5949ee9601cf07ed27c067760135
Błażej Czekała <blazej.czekala@gmail.com>
2019-12-11 12:05:46 +0100

    todos

--- src/main/java/karta_pacjenta/pacjent_service/ServiceApplication.java
+++ src/main/java/karta_pacjenta/pacjent_service/ServiceApplication.java
@@ -3,6 +3,15 @@ package karta_pacjenta.pacjent_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO
+ * 		- dokumentacja
+ * 		- sql
+ * 		- help
+ * 		- timestamp + funkcja skrótu
+ * 				- serwer w Borowcu
+ * 		- QR code generator
+ */
 @SpringBootApplication
 public class ServiceApplication {
 
