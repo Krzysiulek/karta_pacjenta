d0a28aeb7428edf7628277d81d57d7f7cb3ac0be
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-12-11 11:04:50 +0100

    doctors contorller fixed

--- src/main/java/karta_pacjenta/pacjent_service/Controllers/DoctorController.java
+++ src/main/java/karta_pacjenta/pacjent_service/Controllers/DoctorController.java
@@ -1,4 +1,26 @@
 package karta_pacjenta.pacjent_service.Controllers;
 
+import karta_pacjenta.pacjent_service.Models.DAOs.Entities.Doctor;
+import karta_pacjenta.pacjent_service.Repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/doctors")
 public class DoctorController {
+    @Autowired
+    private DoctorRepository diseasesRepository;
+
+
+    @GetMapping()
+    public List<Doctor> getAllDoctors() {
+        return diseasesRepository.findAll();
+    }
+
 }
