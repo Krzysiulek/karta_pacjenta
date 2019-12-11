91321046b6b4b583525b2935e19d2f7b66f8d918
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-12-11 22:54:29 +0100

    Fixed getting all patients DTO. Time improvement

--- src/main/java/karta_pacjenta/pacjent_service/Models/DTOs/PatientInfoTO.java
+++ src/main/java/karta_pacjenta/pacjent_service/Models/DTOs/PatientInfoTO.java
@@ -1,10 +1,12 @@
 package karta_pacjenta.pacjent_service.Models.DTOs;
 
+import karta_pacjenta.pacjent_service.Utils.StringEncrypt;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.HashSet;
@@ -18,12 +20,20 @@ public class PatientInfoTO {
     @Id
     private long patientId;
     private long userId;
+
     private String userName;
+    @Convert(converter = StringEncrypt.class)
     private String email;
     private HashSet<String> roles;
+
+    @Convert(converter = StringEncrypt.class)
     private String firstName;
+    @Convert(converter = StringEncrypt.class)
     private String lastName;
+    @Convert(converter = StringEncrypt.class)
     private String address;
+    @Convert(converter = StringEncrypt.class)
     private String phoneNumber;
+    @Convert(converter = StringEncrypt.class)
     private String personalIdentityNumber;
 }
